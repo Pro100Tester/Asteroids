c13d16e7a3178f3cbe842052d7b6ba30d83621f1
Pro100Tester <90908272+Pro100Tester@users.noreply.github.com>
2021-09-17 16:49:00 +0500

    Update Vector2Extended.cs

--- Assets/Scripts/Extended/Vector2Extended.cs
+++ Assets/Scripts/Extended/Vector2Extended.cs
@@ -6,8 +6,6 @@ namespace Extended
     {
         public static Vector2 Clamp(this ref Vector2 value, in Vector2 Min, in Vector2 Max)
         {
-            value.x = Mathf.Clamp(value.x, Min.x, Max.x);
-            value.y = Mathf.Clamp(value.y, Min.y, Max.y);
             return value;
         }
     }
